0ada2771d95373e05ea7f28f51c673b79c7e0452
apjanco <apjanco@gmail.com>
2025-08-04 13:40:00 -0400

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+const { EleventyI18nPlugin } = require("@11ty/eleventy");
+const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language");
+const yaml = require("js-yaml");
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(EleventyI18nPlugin, {
+      // any valid BCP 47-compatible language tag is supported
+      defaultLanguage: "en", // Required, this site uses "en"
+      errorMode: "never"
+    });
+    eleventyConfig.addPlugin(inclusiveLangPlugin);
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+    // Copy Static Files to /_Site
+    eleventyConfig.addPassthroughCopy({
+    "./admin/config.yml": "./admin/config.yml",
+    });
+    
+  };
